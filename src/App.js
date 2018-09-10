69b8b13e2635467fc84d4162aa25f463652f85df
fabgrondin <bart.rapia@gmail.com>
2018-09-10 09:09:50 +0200

    Feedback matched-mismatched letter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Letter from './Letter.js';
 import './App.css';
 
-const listOfWords = ["arbre", "chat", "abricot", "jupe", "tournevis"];
+const LIST_OF_WORDS = ["arbre", "chat", "abricot", "jupe", "tournevis"];
+const VISUAL_DELAY = 750
 
 class App extends Component {
   state = {
     word: this.getWordToFind(),
     guesses: 0,
+    currentLetter: "",
     usedLetters: new Set()
   }
   checkLetter = (letter) => {
@@ -21,16 +23,28 @@ class App extends Component {
 
     this.setState({
       guesses: guesses,
+      currentLetter: letter,
       usedLetters: usedLetters
     });
+
+    setTimeout(() => this.setState({currentLetter: ""}), VISUAL_DELAY)
   }
   computeDisplay(phrase) {
     return phrase.replace(/\w/g,
       (letter) => (this.state.usedLetters.has(letter) ? letter : '_')
     )
   }
+  getFeedback(letter) {
+    const { currentLetter, usedLetters } = this.state
+
+    if (currentLetter === letter) {
+      return usedLetters.has(letter) ? "matched" : "mismatched"
+    }
+
+    return usedLetters.has(letter) ? "used" : ""
+  }
   getWordToFind() {
-    return listOfWords[Math.floor(Math.random()*listOfWords.length)].toUpperCase();
+    return LIST_OF_WORDS[Math.floor(Math.random()*LIST_OF_WORDS.length)].toUpperCase();
   }
   resetGame() {
     this.setState({
@@ -40,7 +54,7 @@ class App extends Component {
     })
   }
   render() {
-    const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    const LETTERS = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     const wordDisplay = this.computeDisplay(this.state.word)
     return (
       <div className="App">
@@ -53,7 +67,7 @@ class App extends Component {
           (wordDisplay.search("_") === -1) ?
             (<button id="reset" onClick={()=>this.resetGame()}>Reset</button>)
             :
-            letters.map((letter,index) => (<Letter letter={letter} key={index} isUsed={this.state.usedLetters.has(letter)} onClick={this.checkLetter} />))
+            LETTERS.map((letter,index) => (<Letter letter={letter} key={index} feedback={this.getFeedback(letter)} onClick={this.checkLetter} />))
         }
         </div>
       </div>
--- src/Letter.js
+++ src/Letter.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Letter.css'
 
-const Letter = ({ letter, isUsed, onClick }) => (
-  <div className={"letter" + (isUsed ? " used" : "")} onClick={()=>onClick(letter)}>
+const Letter = ({ letter, feedback, onClick }) => (
+  <div className={"letter " + feedback} onClick={()=>onClick(letter)}>
     <span>{letter}</span>
   </div>
 )
